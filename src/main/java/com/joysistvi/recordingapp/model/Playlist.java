c77b998edc0db6c23fe6c3e99a8bc1bb27121fbb
Lester <lstrosana.18@gmail.com>
2026-07-29 03:21:09 +0800

    Refactor Playlist class constructors to remove unused parameters and improve clarity

--- src/main/java/com/joysistvi/recordingapp/model/Playlist.java
+++ src/main/java/com/joysistvi/recordingapp/model/Playlist.java
@@ -3,29 +3,29 @@ package com.joysistvi.recordingapp.model;
 // Model Class
 // Stores the information of a Playlist object using encapsulation
 public class Playlist {
-	
+
 	// Private fields (Encapsulation)
 	private int id;
 	private String playlistName;
 	private int userId;
-	
+
 	// Default constructor
 	public Playlist() {
 	}
-	
+
 	// Constructor for existing methods (includes ID)
 	public Playlist(int id, String playlistName, int userId) {
 		this.id = id;
 		this.playlistName = playlistName;
 		this.userId = userId;
 	}
-	
+
 	// Constructor for creating a new playlist
-	public Playlist(String playlistName, String dateCreated, int userId, String createdAt) {
+	public Playlist(String playlistName, int userId) {
 		this.playlistName = playlistName;
 		this.userId = userId;
 	}
-	
+
 	// Getter (Accessor) and Setter (Mutator) Methods
 
 	public int getId() {
