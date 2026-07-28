19fe52962f643472dbdad66d9a975566f25caf99
Lester <lstrosana.18@gmail.com>
2026-07-29 02:31:57 +0800

    Refactor Playlist class to remove unused fields and simplify constructors

--- src/main/java/com/joysistvi/recordingapp/model/Playlist.java
+++ src/main/java/com/joysistvi/recordingapp/model/Playlist.java
@@ -7,29 +7,23 @@ public class Playlist {
 	// Private fields (Encapsulation)
 	private int id;
 	private String playlistName;
-	private String dateCreated;
 	private int userId;
-	private String createdAt;
 	
 	// Default constructor
 	public Playlist() {
 	}
 	
 	// Constructor for existing methods (includes ID)
-	public Playlist(int id, String playlistName, String dateCreated, int userId, String createdAt) {
+	public Playlist(int id, String playlistName, int userId) {
 		this.id = id;
 		this.playlistName = playlistName;
-		this.dateCreated = dateCreated;
 		this.userId = userId;
-		this.createdAt = createdAt;
 	}
 	
 	// Constructor for creating a new playlist
 	public Playlist(String playlistName, String dateCreated, int userId, String createdAt) {
 		this.playlistName = playlistName;
-		this.dateCreated = dateCreated;
 		this.userId = userId;
-		this.createdAt = createdAt;
 	}
 	
 	// Getter (Accessor) and Setter (Mutator) Methods
@@ -50,14 +44,6 @@ public class Playlist {
 		this.playlistName = playlistName;
 	}
 
-	public String getDateCreated() {
-		return dateCreated;
-	}
-
-	public void setDateCreated(String dateCreated) {
-		this.dateCreated = dateCreated;
-	}
-
 	public int getUserId() {
 		return userId;
 	}
@@ -66,11 +52,4 @@ public class Playlist {
 		this.userId = userId;
 	}
 
-	public String getCreatedAt() {
-		return createdAt;
-	}
-
-	public void setCreatedAt(String createdAt) {
-		this.createdAt = createdAt;
-	}
 }
