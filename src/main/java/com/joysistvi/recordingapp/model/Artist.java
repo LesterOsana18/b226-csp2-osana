9118a5c2a555820c5530dee0e219a502b1711a2c
Lester <lstrosana.18@gmail.com>
2026-07-29 00:37:32 +0800

    Remove createdAt field and related constructors from Artist class for simplification

--- src/main/java/com/joysistvi/recordingapp/model/Artist.java
+++ src/main/java/com/joysistvi/recordingapp/model/Artist.java
@@ -7,23 +7,20 @@ public class Artist {
 	// Private fields (Encapsulation)
 	private int id;
 	private String artistName;
-	private String createdAt;
 	
 	// Default constructor
 	public Artist() {
 	}
 	
 	// Constructor for existing records (includes ID)
-	public Artist(int id, String artistName, String createdAt) {
+	public Artist(int id, String artistName) {
 		this.id = id;
 		this.artistName = artistName;
-		this.createdAt = createdAt;
 	}
 	
 	// Constructor for creating a new artist
-	public Artist(String artistName, String createdAt) {
+	public Artist(String artistName) {
 		this.artistName = artistName;
-		this.createdAt = createdAt;
 	}
 	
 	// Getter (Accessor) and Setter (Mutator) Methods
@@ -42,13 +39,5 @@ public class Artist {
 
 	public void setArtistName(String artistName) {
 		this.artistName = artistName;
-	}
-
-	public String getCreatedAt() {
-		return createdAt;
-	}
-
-	public void setCreatedAt(String createdAt) {
-		this.createdAt = createdAt;
 	}
 }
